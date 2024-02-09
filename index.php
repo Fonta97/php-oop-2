0ee5e2758db4b68e9c062013821e31be64a28ac7
Fonta97 <fontana.alessandro97@gmail.com>
2024-02-09 11:17:18 +0100

    icsdì

--- index.php
+++ index.php
@@ -17,5 +17,77 @@ class Product {
         $this->codiceProdotto = $codiceProdotto;
     }
 
+    function printCard() {
+        echo "Name: " . $this->nome . '<br>';
+        echo "Price: " . $this->prezzo . ' euro' . '<br>';
+        echo "Product code: " . $this->codiceProdotto . '<br>';
+    }
 }
+
+class Cibo extends Product {
+    public $tipo;
+    public $peso;
+
+    function __construct($nome, $prezzo, $codiceProdotto, $tipo, $peso) {
+        parent::__construct($nome, $prezzo, $codiceProdotto);
+        $this->tipo = $tipo;
+        $this->peso = $peso;
+    }
+
+    function printCard() {
+        parent::printCard();
+        echo "Type: " . $this->tipo . '<br>';
+        echo "Weight: " . $this->peso;
+    }
+}
+
+class Cucce extends Product {
+    public $materiale;
+    public $dimensione;
+
+    function __construct($nome, $prezzo, $codiceProdotto, $materiale, $dimensione) {
+        parent::__construct($nome, $prezzo, $codiceProdotto);
+        $this->materiale = $materiale;
+        $this->dimensione = $dimensione;
+    }
+
+    function printCard() {
+        parent::printCard();
+        echo "Material: " . $this->materiale . '<br>';
+        echo "Size: " . $this->dimensione;
+    }
+}
+
+class Giochi extends Product {
+    public $materiale;
+    public $forma;
+
+    function __construct($nome, $prezzo, $codiceProdotto, $materiale, $forma) {
+        parent::__construct($nome, $prezzo, $codiceProdotto);
+        $this->materiale = $materiale;
+        $this->forma = $forma;
+    }
+
+    function printCard() {
+        parent::printCard();
+        echo "Material: " . $this->materiale . '<br>';
+        echo "Shape: " . $this->forma . '<br> <br>' ;
+    }
+}
+
+
+// test di un prodotto non trovato
+try {
+    throw new ProdottoInesistente();
+} catch (ProdottoInesistente $e) {
+    echo "Error: " . $e->errorMessage();
+} catch (Exception $e) {
+    echo "Generic Error: " . $e->getMessage();
+}
+
+$gioco = new Giochi('Dinodog', '15', 'X8ESTV', 'Tessuto', 'Dino');
+$gioco->printCard();
+$cuccia = new Cucce('Dog House', '150', 'LELYLA', 'Wood', 'big');
+$cuccia->printCard();
+
 ?>
